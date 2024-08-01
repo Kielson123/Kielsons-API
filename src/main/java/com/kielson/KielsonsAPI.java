f28a8b5da6ff7e4ddd4dc0a9db644a0ba55a765f
Gracek12 <gracjanprzymus12@gmail.com>
2024-08-01 15:20:33 +0200

    Compatibility variable

--- src/main/java/com/kielson/KielsonsAPI.java
+++ src/main/java/com/kielson/KielsonsAPI.java
@@ -3,12 +3,15 @@ package com.kielson;
 import com.kielson.events.KielsonsEvents;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KielsonsAPI implements ModInitializer {
 	public static final String MOD_ID = "kielsonsapi";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	private static final boolean isEnhancedCombatLoaded = FabricLoader.getInstance().isModLoaded("kielsons_enhanced_combat");
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +21,9 @@ public class KielsonsAPI implements ModInitializer {
 			if(original == 0f || livingEntity.getAttributeInstance(KielsonsEntityAttributes.HEALING_MULTIPLIER) == null) return original;
 			return (float) (original * livingEntity.getAttributeValue(KielsonsEntityAttributes.HEALING_MULTIPLIER));
 		});
+	}
+
+	public static boolean isKielsonsEnhancedCombatLoaded(){
+		return isEnhancedCombatLoaded;
 	}
 }
